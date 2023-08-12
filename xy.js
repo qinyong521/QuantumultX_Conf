446f893d0d736b58898a7464e21b456d70134f2e
qinyong521 <70684737+qinyong521@users.noreply.github.com>
2023-08-12 11:40:43 +0800

    Create xy.js

---
+++ xy.js
@@ -0,0 +1,2 @@
+https:\/\/h5\.m\.goofish\.com\/wow\/moyu\/moyu-project\/coin\/pages\/coinRecord url script-request-header https://raw.githubusercontent.com/qinyong521/QuantumultX_Conf/main/gofish_cookie.js
+
